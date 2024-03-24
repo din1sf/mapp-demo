3012f31f3255627143cff28c0a62dd6862be9433
Nikolay Dimitrov <nikolay.dimitrov@bosch.com>
2024-03-24 11:12:35 +0200

    dataframe table

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ import pandas
 
 st.title('Demo App')
 
-
 input_file = st.file_uploader('Upload file',type='csv')
 if input_file is not None:
     st.write('File uploaded')
-    df = pandas.read_csv(input_file, delimiter=';')
-    st.write(df)
+    df = pandas.read_csv(input_file, delimiter=',')    
+
+    st.dataframe(
+    df,
+    hide_index=True,
+    column_config={
+        "id": st.column_config.TextColumn(label="ID"),
+        "owner": st.column_config.TextColumn(label="Owner"),
+        "action": st.column_config.TextColumn(label="Action"),
+    },
+    width=800,
+    )
     st.button('Action Button')
              
 
